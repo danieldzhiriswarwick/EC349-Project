c72d9885568878f959a34e68300dffb467c6518a
danieldzhiriswarwick <daniel.dzhiris@warwick.ac.uk>
2023-11-15 12:01:11 +0000

    Loaded the data sets in the script

---
+++ EC349 Project Script.R
@@ -0,0 +1,14 @@
+#EC349 Project Script
+git 
+#Clear
+cat("\014")  
+rm(list=ls())
+
+#Load Libraries
+library(glmnet)
+library(ggplot2)
+library(tidyverse)
+
+#Loading the small versions of data
+load(file='/Users/danieldzhiris/Downloads/EC349 Project/Assignment/Small Datasets/yelp_review_small.Rda')
+load(file='/Users/danieldzhiris/Downloads/EC349 Project/Assignment/Small Datasets/yelp_user_small.Rda')
