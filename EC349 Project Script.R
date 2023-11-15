7b9a659c7fffc9a92b7cbbf8ff2443ae94f8c918
danieldzhiriswarwick <daniel.dzhiris@warwick.ac.uk>
2023-11-15 18:43:00 +0000

    Split the User Reviews into a training and a test data sets

--- EC349 Project Script.R
+++ EC349 Project Script.R
@@ -8,7 +8,18 @@ rm(list=ls())
 library(glmnet)
 library(ggplot2)
 library(tidyverse)
+library(caret)
 
 #Loading the small versions of data
 load(file='/Users/danieldzhiris/Downloads/EC349 Project/Assignment/Small Datasets/yelp_review_small.Rda')
 load(file='/Users/danieldzhiris/Downloads/EC349 Project/Assignment/Small Datasets/yelp_user_small.Rda')
+
+# Set seed for reproducibility
+set.seed(1)
+
+# Select 10,000 random observations for the training data set
+train_obs <- sample(nrow(review_data_small), 10000)
+train_data <- review_data_small[train_obs, ]
+
+# Select all other observations for the testing data set
+test_data <- review_data_small[-train_obs, ]
