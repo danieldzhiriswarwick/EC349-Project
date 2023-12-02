e27a92825102675c470b40bb5591f2b6784090e0
danieldzhiriswarwick <daniel.dzhiris@warwick.ac.uk>
2023-12-02 18:18:21 +0000

    Added comparison of OOB Error rates for tree-based models

--- EC349 Project Script.R
+++ EC349 Project Script.R
@@ -60,10 +60,26 @@ ggplot(review_data_small, aes(x = stars)) +
        y = "Number of Reviews (million)") +
   scale_y_continuous(labels = scales::number_format(scale = 1e-6, suffix = "M"))
 
+# Calculate the percentage of reviews for each star rating
+percentage_data <- review_data_small %>%
+  group_by(stars) %>%
+  summarise(percentage = n() / nrow(review_data_small) * 100)
+
+# Create a pie chart
+ggplot(percentage_data, aes(x = "", y = percentage, fill = as.factor(stars))) +
+  geom_bar(stat = "identity", width = 1) +
+  geom_text(aes(label = sprintf("%.1f%%", percentage)),
+            position = position_stack(vjust = 0.5), size = 3) +
+  coord_polar("y") +
+  labs(title = "Percentage of Reviews by Star Rating",
+       fill = "Rating") +
+  theme_void() +
+  theme(plot.title = element_text(hjust = 0.5))
+
 #Set seed for reproducibility
-set.seed(1)
-#Selecting random 20,000 observations to make the analysis computationally feasible
-rev_small_index <- sample(nrow(review_data_small), 20000)
+set.seed(123)
+#Selecting random 30,000 observations to make the analysis computationally feasible
+rev_small_index <- sample(nrow(review_data_small), 30000)
 rev_data <- review_data_small[rev_small_index, ]
 
 #Merge with business rating from business_data
@@ -82,7 +98,7 @@ ggplot(rev_data, aes(x = factor(stars), y = business_rating)) +
 rev_data <- rev_data %>%
   left_join(dplyr::select(user_data, user_id, average_stars), by = 'user_id')
 
-#Box-and-whisker plot for thr distribution of the average of ratings given by the user
+#Box-and-whisker plot for the distribution of the average of ratings given by the user
 ggplot(rev_data, aes(x = factor(stars), y = average_stars)) +
   geom_boxplot() +
   labs(x = "Stars", y = "Average of Ratings Given by a User") +
@@ -94,7 +110,7 @@ text_rev_data <- rev_data %>%
   dplyr::select(review_id, text)
 
 #Excluding the following words from the stop_words lexicon.
-words_to_remove <- c("unfortunately", "great", "good", "greatest")
+words_to_remove <- c("unfortunately", "great", "good", "greatest", "matter")
 custom_stop_words <- stop_words %>%
   filter(!word %in% words_to_remove)
 
@@ -104,10 +120,10 @@ review_words <- text_rev_data %>%
   anti_join(custom_stop_words) %>%
   count(review_id, word, sort = TRUE)
 
-#Count the total number of "sentiment" words in each review
+#Count the total number of "sentiment" tokens in each review
 total_words <- review_words %>%
   group_by(review_id) %>%
-  summarize(total = sum(n))
+  summarize(total_tokens = sum(n))
 
 review_words <- left_join(review_words, total_words)
 
@@ -163,16 +179,19 @@ negation_words <- c("not", "no", "never", "without", "isn't", "aren't", "doesn't
 
 negated_words_count <- bigrams_separated %>%
   filter(word1 %in% negation_words) %>%
+  filter(!word2 %in% custom_stop_words$word) %>%
   inner_join(afinn_lexicon, by = c(word2 = "word")) %>%
   count(word1, word2, value, sort = TRUE)
 head(negated_words_count, 10)
 
 negated_words_count %>%
   mutate(contribution = n * value) %>%
-  arrange(desc(abs(contribution))) %>%
+  group_by(word2) %>%
+  summarize(total_contribution = sum(contribution)) %>%
+  arrange(desc(abs(total_contribution))) %>%
   head(20) %>%
-  mutate(word2 = reorder(word2, contribution)) %>%
-  ggplot(aes(n * value, word2, fill = n * value > 0)) +
+  mutate(word2 = reorder(word2, total_contribution)) %>%
+  ggplot(aes(total_contribution, word2, fill = total_contribution > 0)) +
   geom_col(show.legend = FALSE) +
   labs(x = "Sentiment value * number of occurrences",
        y = "Words preceded by negation words")
@@ -180,6 +199,7 @@ negated_words_count %>%
 #AFINN Values to be reversed due to the presence of negation words
 negated_words <- bigrams_separated %>%
   filter(word1 %in% negation_words) %>%
+  filter(!word2 %in% custom_stop_words$word) %>%
   inner_join(afinn_lexicon, by = c(word2 = "word"))
 
 #Summarising total AFINN Values to be reversed due to the presence of negation words by review
@@ -211,7 +231,7 @@ afinn_review_sent <- merge(x = afinn_review_sent, y = rev_data[, c("review_id", 
 
 #Adding a column for the total number of words in each review
 afinn_review_sent <- left_join(afinn_review_sent, review_words %>% 
-                                 dplyr::select(review_id, total), by = "review_id") %>%
+                                 dplyr::select(review_id, total_tokens), by = "review_id") %>%
   distinct(review_id, .keep_all = TRUE)
 
 #Adding a column with reversed sentiment of words that are preceded by negation words
@@ -224,27 +244,27 @@ afinn_review_sent <- afinn_review_sent %>%
 #Normalising sentiment score for the total number of words in a review
 afinn_review_sent <- afinn_review_sent %>%
   group_by(review_id) %>%
-  mutate(afinn_sentiment_norm = sum(sentiment_adjusted / total, na.rm = TRUE))
+  mutate(afinn_sentiment_norm = sum(sentiment_adjusted / total_tokens, na.rm = TRUE))
 
 #Merge back the business_rating and average_stars variables 
 afinn_review_sent <- afinn_review_sent %>%
   left_join(dplyr::select(rev_data, review_id, business_rating, average_stars), by = 'review_id')
 
 #Exploring the relationship between the length of a review and the stars rating
-ggplot(afinn_review_sent, aes(x = total, y = stars)) +
+ggplot(afinn_review_sent, aes(x = total_tokens, y = stars)) +
   geom_point(color = "blue", size = 3) +
   geom_smooth(aes(group = 1), method = "lm", se = FALSE, color = "red", linetype = "solid") +
   labs(title = "Scatter Plot of Total vs. Stars",
        x = "Length of Review", y = "Stars")
 
-ggplot(afinn_review_sent, aes(x = total, y = stars)) +
+ggplot(afinn_review_sent, aes(x = total_tokens, y = stars)) +
   geom_smooth(aes(group = 1), method = "lm", se = FALSE, color = "red", linetype = "solid") +
   labs(title = "Regression Line of Lenth of Review vs. Stars",
        x = "Length of Review", y = "Stars")
 
 #Checking for multicollinearity among the chosen independent variables
 independent_var <- afinn_review_sent[, c("afinn_sentiment_norm", "business_rating",
-                                         "average_stars", "total")]
+                                         "average_stars", "total_tokens")]
 correlation_matrix <- cor(independent_var)
 print(correlation_matrix)
 
@@ -252,11 +272,12 @@ print(correlation_matrix)
 afinn_review_sent$stars <- as.factor(afinn_review_sent$stars)
 
 #Set seed for reproducibility
-set.seed(2)
-#Split data into 50% training and 50% testing
-parts = createDataPartition(afinn_review_sent$stars, p = 0.5, list = F)
-train_data = afinn_review_sent[parts, ]
-test_data = afinn_review_sent[-parts, ]
+set.seed(1)
+#Split data so that the test data set has 10,000 random observations
+test_indices <- sample(1:nrow(afinn_review_sent), 10000, replace = FALSE)
+test_data <- afinn_review_sent[test_indices, ]
+# Use the remaining observations for the training set
+train_data <- afinn_review_sent[-test_indices, ]
 
 # Decision Tree
 rpart_tree <- rpart(stars ~ afinn_sentiment_norm+business_rating+average_stars, data = train_data)
@@ -277,9 +298,9 @@ bag_accuracy <- sum(diag(bag_confusion_matrix)) / sum(bag_confusion_matrix)
 print(paste("Accuracy:", round(bag_accuracy, 2)))
 
 # Random Forest
-set.seed(1234)
-rf_tree <- randomForest(stars ~ afinn_sentiment_norm+business_rating+average_stars+total,
-                        data = train_data, ntree = 100, importance = TRUE)
+set.seed(2)
+rf_tree <- randomForest(stars ~ afinn_sentiment_norm+business_rating+average_stars+total_tokens,
+                        data = train_data, ntree = 100, mtry = 1, importance = TRUE)
 rf_predictions <- predict(rf_tree, newdata = test_data, type = "class")
 rf_confusion_matrix <- table(rf_predictions, test_data$stars)
 print(rf_confusion_matrix)
@@ -289,37 +310,16 @@ mean(rf_tree[["err.rate"]])
 importance(rf_tree)
 varImpPlot(rf_tree, main = "Variable Importance Plot")
 plot(rf_tree)
-test_data <- cbind(test_data, rf_predictions)
+# Null rate
+null_rate <- max(table(test_data$stars)) / length(test_data$stars)
+print(paste("Null Rate:", round(null_rate, 2)))
 
-# Ridge (ONLY FOR CONTINUOUS)
-x_train <- train_data[, c("afinn_sentiment_norm", "business_rating", "average_stars")]
-y_train <- train_data$stars
-x_test <- test_data[, c("afinn_sentiment_norm", "business_rating", "average_stars")]
-y_test <- test_data$stars
-#Ridge on Train Dataset, setting lambda = 3
-ridge_mod<-glmnet(x_train, y_train, alpha = 0, lambda = 3, thresh = 1e-12)
-coef(ridge_mod) #coefficients
-#Ridge with Cross-Validation - the cv. packages requires "matrix" rather than dataframe
-cv.out <- cv.glmnet(as.matrix(x_train), as.matrix(y_train), alpha = 0, nfolds = 3)
-plot(cv.out)
-lambda_ridge_cv <- cv.out$lambda.min #cross-validation is the lambda minimising empirical MSE in training data
-#Re-Estimate Ridge with lambda chosen by Cross validation
-ridge_mod2<-glmnet(x_train, y_train, alpha = 0, lambda = lambda_ridge_cv, thresh = 1e-12)
-#Fit on Test Data
-ridge_pred <- predict(ridge_mod2, s = lambda_ridge_cv, newx = as.matrix(x_test))
-ridge_MSE<- mean((ridge_pred - y_test) ^ 2)
-print(paste("Mean Squared Error (MSE):", round(ridge_MSE, 2)))
-rounded_ridge_predictions <- round(ridge_pred)
-rounded_confusion_matrix_ridge <- table(rounded_ridge_predictions, y_test)
-print(rounded_confusion_matrix_ridge)
-rounded_accuracy_ridge <- sum(diag(rounded_confusion_matrix_ridge)) / sum(rounded_confusion_matrix_ridge)
-print(paste("Accuracy:", round(rounded_accuracy_ridge, 2)))
-rounded_mse_ridge <- mean((rounded_ridge_predictions - y_test)^2)
-print(paste("Mean Squared Error (MSE):", round(rounded_mse_ridge, 2)))
+#Combining the test data with the predicted star ratings to examine observations with incorrect predictions
+test_data <- cbind(test_data, rf_predictions)
 
 # Ordered probit
 # Fit ordered probit model on train_data
-ordered_probit <- polr(stars ~ afinn_sentiment_norm + business_rating + average_stars + total,
+ordered_probit <- polr(stars ~ afinn_sentiment_norm + business_rating + average_stars + total_tokens,
                        data = train_data, method = "probit")
 # Predict stars on test_data
 probit_predictions <- predict(ordered_probit, newdata = test_data, type = "class")
@@ -328,3 +328,45 @@ probit_confusion_matrix <- table(probit_predictions, test_data$stars)
 print(probit_confusion_matrix)
 probit_accuracy <- sum(diag(probit_confusion_matrix)) / sum(probit_confusion_matrix)
 print(paste("Accuracy:", round(probit_accuracy, 2)))
+
+# Comparison of OOB Error for tree-based methods
+# Set your values for m (number of predictors to consider at each split)
+m_values <- c(4, 2, 1)  # Updated values
+# Set the initial and final number of trees, as well as the increment
+initial_trees <- 1
+final_trees <- 101
+tree_increment <- 10
+# Create an empty data frame to store results
+results_df <- data.frame()
+# Loop through different values of m
+for (m in m_values) {
+  # Loop through different numbers of trees
+  for (ntree in seq(initial_trees, final_trees, by = tree_increment)) {
+    # Create and train a Random Forest model
+    rf_model <- randomForest(factor(stars) ~ afinn_sentiment_norm+business_rating+average_stars+total_tokens, 
+                             data = train_data, 
+                             mtry = m, 
+                             ntree = ntree,
+                             importance = TRUE)
+    
+    # Extract test classification error
+    error_rate <- mean(rf_model[["err.rate"]][, "OOB"])
+    
+    # Create a data frame with results
+    results_df <- rbind(results_df, data.frame(m = rep(m, 1), 
+                                               ntree = ntree, 
+                                               test_error = error_rate))
+  }
+}
+
+# Plot the results
+ggplot(results_df, aes(x = ntree, y = test_error, color = factor(m))) +
+  geom_line() +
+  labs(x = "Number of Trees",
+       y = "Out-of-Bag (OOB) Error") +
+  scale_color_manual(name = "",
+                     values = c("4" = "blue", "2" = "green", "1" = "red"),
+                     labels = c("4" = "m = p = 4", "2" = "m = 2", "1" = "m = 1")) +
+  theme(legend.position = c(0.85, 0.9),
+        panel.grid = element_blank(),
+        panel.background = element_blank())
